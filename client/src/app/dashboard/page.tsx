d5be158bdb91f45c9660b9959ccd45e1eb87b490
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-27 03:22:30 -0400

    Agregar página de inicio de administrador al cliente

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,10 +1,11 @@
+import DefaultCard from "@/components/admin/DefaultCard";
 
 const AdminHomePage = () => {
     return (
-
-        <main>
-        </main>
-
+        <div className="grid grid-cols-6 gap-4">
+            <DefaultCard title='Clients' counter={2} link='/dashboard/clients' />
+            <DefaultCard title='Projects' counter={7} link='/dashboard/projects' />
+        </div>
     );
 }
 
