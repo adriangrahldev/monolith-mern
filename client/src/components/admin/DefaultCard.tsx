61827a28df19d8dbf60e29f56e077186355f2d4c
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-27 15:24:47 -0400

    Agregar propiedad subtitle a DefaultCard y ajustar diseño

--- client/src/components/admin/DefaultCard.tsx
+++ client/src/components/admin/DefaultCard.tsx
@@ -4,12 +4,14 @@ import { ChevronRightIcon } from "@radix-ui/react-icons";
 
 const DefaultCard = ({
   title,
+  subtitle,
   description,
   counter,
   link,
   theme = "light",
 }: {
   title: string;
+  subtitle?: string;
   description?: string;
   counter: number;
   link: string;
@@ -17,18 +19,22 @@ const DefaultCard = ({
 }) => {
   return (
     <Card className={`${theme === 'dark' ? 'bg-black text-white' : 'bg-white text-black'}`}>
-      <CardHeader>
+      <CardHeader className="py-4">
         <div className="flex justify-between items-center">
-          <h2 className="text-xl font-bold">{title}</h2>
-          <Link href={link}>
+          <div>
+            <h2 className="text-xl font-bold">{title}</h2>
+            {subtitle && <h3 className="text-sm font-semibold text-slate-500">{subtitle}</h3> }
+          </div>
+
+          <Link href={link} className="self-start">
             <ChevronRightIcon className="w-6 h-6" />
           </Link>
         </div>
       </CardHeader>
       <CardContent>
-        <div className="flex justify-between">
-          <p>{description}</p>
-          <span className="text-4xl font-bold">{counter}</span>
+        <div className="flex gap-2 justify-between">
+          <p className="text-sm">{description}</p>
+          <span className="text-4xl font-bold self-end">{counter}</span>
         </div>
       </CardContent>
     </Card>
