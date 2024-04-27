32f54e995540e01b58cf3a5e4bd9a42a96039151
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-27 03:56:22 -0400

    Agregar componente TaskTable al cliente

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import DefaultCard from "@/components/admin/DefaultCard";
+import TaskTable from "@/components/tasks/TaskTable";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Table, TableCaption, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table";
 import { useEffect, useState } from "react";
 
 const AdminHomePage = () => {
@@ -42,38 +42,6 @@ const AdminHomePage = () => {
                 project: "Web Design",
                 status: "Not started"
             },
-            {
-                id: 5,
-                description: "Design new homepage",
-                dateFrom: "24 May 2024, 10:00",
-                dateTo: "27 May 2024, 09:30",
-                project: "Web Design",
-                status: "Not started"
-            },
-            {
-                id: 6,
-                description: "Design services page",
-                dateFrom: "28 May 2024, 10:00",
-                dateTo: "30 May 2024, 09:30",
-                project: "Web Design",
-                status: "Not started"
-            },
-            {
-                id: 7,
-                description: "Design about us page",
-                dateFrom: "31 May 2024, 10:00",
-                dateTo: "4 June 2024, 09:30",
-                project: "Web Design",
-                status: "Not started"
-            },
-            {
-                id: 8,
-                description: "Design contact us page",
-                dateFrom: "5 June 2024, 10:00",
-                dateTo: "7 June 2024, 09:30",
-                project: "Web Design",
-                status: "Not started"
-            },
         ])
     }, []);
   return (
@@ -90,34 +58,7 @@ const AdminHomePage = () => {
         <h1 className="text-2xl font-bold">Upcoming tasks</h1>
         </CardHeader>
         <CardContent>
-
-            <Table>
-            <TableCaption>A list of your upcoming tasks.</TableCaption>
-            <TableHeader>
-                <TableRow>
-                <TableHead className="w-[400px]">Description</TableHead>
-                <TableHead>Date</TableHead>
-                <TableHead>Project</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Actions</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody className="overflow-scroll">
-                {
-                    pendingTasks.map((task: any) => (
-                        <TableRow key={task.id}>
-                            <TableCell>{task.description}</TableCell>
-                            <TableCell>{task.dateFrom}</TableCell>
-                            <TableCell>{task.project}</TableCell>
-                            <TableCell>{task.status}</TableCell>
-                            <TableCell>
-                                <button className="btn btn-primary">Start</button>
-                            </TableCell>
-                        </TableRow>
-                    ))
-                }
-            </TableBody>
-            </Table>
+            <TaskTable tasks={pendingTasks} ></TaskTable>
         </CardContent>
       </Card>
     </div>
