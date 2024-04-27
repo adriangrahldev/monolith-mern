f96a5b145160fcb16e999eb5b7a6a7929b96b441
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-27 02:46:57 -0400

    Agregar componente UserBadge al cliente

---
+++ client/src/components/user/UserBadge.tsx
@@ -0,0 +1,18 @@
+
+const UserBadge = ({user}:{user:any}) => {
+    return (
+        <div className="flex gap-2 items-center justify-center rounded-md bg-slate-200 px-4 py-2 ">
+            <div className="">
+                <div className="w-[43px] h-[43px] bg-black rounded-full flex items-center justify-center">
+                    <span className="text-white font-bold text-lg">{user.name[0]}</span>
+                </div>
+            </div>
+            <div className="flex flex-col -space-y-1">
+                <span className="font-semibold">{user.name}</span>
+                <span className="text-sm">{user.email}</span>
+            </div>
+        </div>
+    )
+}
+
+export default UserBadge;
