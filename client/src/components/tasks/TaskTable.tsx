99d56a24e23bbaa2221759bf64baf5172aa8ef8f
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-27 03:56:07 -0400

    Agregar componente TaskTable al cliente

---
+++ client/src/components/tasks/TaskTable.tsx
@@ -0,0 +1,33 @@
+import { Table, TableCaption, TableHeader, TableRow, TableHead, TableBody, TableCell } from "../ui/table";
+
+export const TaskTable = ({tasks}:{tasks: any[]}) => {
+  return (
+    <Table>
+      <TableCaption>A list of your upcoming tasks.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[400px]">Description</TableHead>
+          <TableHead>Date</TableHead>
+          <TableHead>Project</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody className="overflow-scroll">
+        {tasks.map((task: any) => (
+          <TableRow key={task.id}>
+            <TableCell>{task.description}</TableCell>
+            <TableCell>{task.dateFrom}</TableCell>
+            <TableCell>{task.project}</TableCell>
+            <TableCell>{task.status}</TableCell>
+            <TableCell>
+              <button className="btn btn-primary">Start</button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default TaskTable;
