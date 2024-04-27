9b39a0fd76c41358afed8d5c850d9ada468131d6
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-27 04:07:34 -0400

    Modificar diseño del layout en layout.tsx

--- client/src/app/(projects)/layout.tsx
+++ client/src/app/(projects)/layout.tsx
@@ -1,35 +1,25 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { SideBar } from "@/components/ui/SideBar";
-import { Header } from "@/components/ui/Header";
+import Sidebar from "@/components/admin/Sidebar";
+import TopBar from "@/components/admin/TopBar";
+import { ReactNode } from "react";
+import { routes } from "@/routes";
+import { BreadcrumbProvider, useBreadcrumb } from "@/contexts/BreadcrumbContext";
+const AdminLayout = ({children} : {children: ReactNode}) =>{
+    
+    
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Monolith - Freelancer project management tool",
-  description: "Monolith is a project management tool for freelancers. It helps you to manage your projects, clients, and invoices in one place.",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <div className={`font-inter min-h-screen flex flex-col ${inter.className}`}>
-      <div className="flex flex-row min-h-screen">
-        <div className="bg-gray-200">
-          <SideBar />
-        </div>
-        <div className="flex-1 flex flex-col">
-          <div className="flex justify-end w-100 mt-4 mr-4">
-            <Header />
-          </div>
-          <div className="flex-1">
-            {children}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    return(
+        <BreadcrumbProvider>
+            <div className="flex">
+                <Sidebar routes={routes} />
+                <main className="w-full">
+                    <TopBar/>
+                    <div className="p-6 mx-20 my-20">   
+                        {children}
+                    </div>
+                </main>
+            </div>
+        </BreadcrumbProvider>
+    )
 }
+
+export default AdminLayout;
