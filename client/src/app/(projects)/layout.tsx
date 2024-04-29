95d4f61c0b40a549d83c32cbe3385f455444d5af
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-29 03:21:03 -0400

    Agregar funcionalidad de toggleSidebar en el layout de proyectos

--- client/src/app/(projects)/layout.tsx
+++ client/src/app/(projects)/layout.tsx
@@ -1,19 +1,24 @@
+"use client"
 import { SideBar } from "@/components/ui/SideBar";
 import TopBar from "@/components/ui/TopBar";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { routes } from "@/routes";
 import { BreadcrumbProvider, useBreadcrumb } from "@/contexts/BreadcrumbContext";
 const AdminLayout = ({ children }: { children: ReactNode }) => {
 
+    const [showingSidebar, setShowingSidebar] = useState(false);
 
+    const toggleSidebar = () => {
+        setShowingSidebar(!showingSidebar);
+    }
 
     return (
         <BreadcrumbProvider>
             <div className="flex">
-                <SideBar routes={routes} />
+                <SideBar routes={routes} toggleSidebar={toggleSidebar} showingSidebar={showingSidebar} />
                 <main className="w-full">
-                    <TopBar />
-                    <div className="p-6 ml-20 mt-20">
+                    <TopBar toggleSidebar={toggleSidebar} />
+                    <div className="p-6 ml-20 max-lg:ml-0 mt-20">
                         <div className="flex flex-col gap-8">
                             {children}
                         </div>
