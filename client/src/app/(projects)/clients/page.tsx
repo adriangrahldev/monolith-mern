665a0dcf1be24a7ab46157638632175a077c359b
José Ferreira <jf561428@gmail.com>
2024-04-29 00:13:48 -0400

    Implement client page

--- client/src/app/(projects)/clients/page.tsx
+++ client/src/app/(projects)/clients/page.tsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import DefaultCard from "@/components/admin/DefaultCard";
+import { Button } from "@/components/ui/button";
+import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
- const page = () => {
+
+const page = () => {
+  const clients = [
+    {
+      _id: "a67b1a3b3b73e6d1e0b4e",
+      name: 'Adrian Grahl',
+      totalProjects: 5,
+      avatar: 'https://randomuser.me/api/portraits/men/77.jpg'
+    },
+    {
+      _id: "a67b1a3b3b73e6d1e0b4e",
+      name: 'Adrian Grahl',
+      totalProjects: 5,
+      avatar: 'https://randomuser.me/api/portraits/men/78.jpg'
+
+    },
+    {
+      _id: "a67b1a3b3b73e6d1e0b4e",
+      name: 'Adrian Grahl',
+      totalProjects: 5,
+    },
+    {
+      _id: "a67b1a3b3b73e6d1e0b4e",
+      name: 'Adrian Grahl',
+      totalProjects: 5,
+    },
+    {
+      _id: "a67b1a3b3b73e6d1e0b4e",
+      name: 'Adrian Grahl',
+      totalProjects: 5,
+    },
+
+
+  ]
   return (
-    <div>page</div>
+    <>
+      <div id='toolbar' className='w-full flex gap-2 items-center bg-gray-200 rounded-md px-2 h-12 '>
+        <Button variant={'ghost'}>
+          <FontAwesomeIcon icon={faPlusSquare} className='mr-2' />
+          Add Client
+        </Button>
+      </div>
+      <hr />
+      <div id='projects-container' className='grid xl:grid-cols-4 grid-rows-3 gap-4 rounded-md lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 '>
+        {clients.map((client, index) => (
+          <DefaultCard key={index} title={client.name} counter={Math.round(Math.random() * 100)} counterText={'Projects'} avatar={client.avatar} link={`/clients/${client._id}`} />
+        ))}
+      </div>
+    </>
+
   )
 }
 
