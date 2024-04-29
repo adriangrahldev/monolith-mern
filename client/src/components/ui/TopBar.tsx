99e3e63d067d3dfd1819f7c7877bed557b8e6187
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-29 02:23:03 -0400

    Agregar botón toggleSidebar que solo se muestra en dispositivos móviles

--- client/src/components/ui/TopBar.tsx
+++ client/src/components/ui/TopBar.tsx
@@ -12,64 +12,102 @@ import { useBreadcrumb } from "@/contexts/BreadcrumbContext";
 import UserBadge from "../user/UserBadge";
 import { Fragment, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronRight, faCircleDot } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faChevronRight,
+  faCircleDot,
+} from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import { Button } from "./button";
 
-const TopBar = () => {
+const TopBar = ({ toggleSidebar }: { toggleSidebar: CallableFunction }) => {
   const { breadcrumb, addItem, removeItem } = useBreadcrumb();
   const [user, setUser] = useState<undefined | any>(undefined);
+  const [showMobileBreadcrumb, setShowMobileBreadcrumb] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
       setUser({
         name: "John Doe",
-        email: "contact@adriangrahl.dev"
+        email: "contact@adriangrahl.dev",
       });
     }, 2000);
+
+    const handleResize = () => {
+      setShowMobileBreadcrumb(window.innerWidth < 640);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   return (
-    <div className="w-full pr-20 h-20 ml-20 fixed bg-white border-b-2">
-      <div className="h-20 flex justify-between items-center px-6 ">
-        <div className="topLeft flex gap-2 items-center text-2xl">
-          <div className={`font-bold text-black`}>
-            Dashboard
+    <div className="w-full pr-4 md:pr-20 h-20 ml-4 md:ml-20 fixed bg-white border-b-2">
+      <div className="h-20 flex justify-between items-center px-2 md:px-6">
+        <div className="topLeft flex gap-2 items-center text-lg md:text-2xl">
+          <div
+            className={`font-bold text-black md:hidden sm:hidden flex items-center`}
+          >
+            <Button
+              variant={"ghost"}
+              className="p-0 m-0"
+              onClick={(e) => toggleSidebar()}
+            >
+              <FontAwesomeIcon icon={faBars} />
+            </Button>
           </div>
+          <div className={`font-bold text-black`}>Dashboard</div>
           {breadcrumb.map((item: BCItem, index: number) => (
             <Fragment key={index}>
-              <span><FontAwesomeIcon icon={faChevronRight} width={8}  /></span>
-              <div className={`font-normal text-lg text-gray-600`}>
-                {item.type === "link" ? (
-                  <BreadcrumbItem key={index}>
-                    <Link href={item.link || '/dashboard'}>
-                    {item.icon}
-                    {item.title}
-                    </Link>
-                  </BreadcrumbItem>
-                ) : item.type === "dropdown" ? (
-                  <BreadcrumbItem key={index}>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger className="flex breadcrumb-center gap-1">
-                        {item.title}
-                        <ChevronDownIcon />
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="start">
-                        {item.items?.map((item: any, index: number) => (
-                          <DropdownMenuItem key={index}>{item}</DropdownMenuItem>
-                        ))}
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </BreadcrumbItem>
-                ) : (
-                  <BreadcrumbItem key={index}>{item.title}</BreadcrumbItem>
-                )}
-              </div>
+              {showMobileBreadcrumb && index > 0 ? null : (
+                <Fragment>
+                  <span>
+                    <FontAwesomeIcon icon={faChevronRight} width={8} />
+                  </span>
+                  <div
+                    className={`font-normal text-sm md:text-lg text-gray-600`}
+                  >
+                    {item.type === "link" ? (
+                      <BreadcrumbItem key={index}>
+                        <Link href={item.link || "/dashboard"}>
+                          {item.icon}
+                          {item.title}
+                        </Link>
+                      </BreadcrumbItem>
+                    ) : item.type === "dropdown" ? (
+                      <BreadcrumbItem key={index}>
+                        <DropdownMenu>
+                          <DropdownMenuTrigger className="flex breadcrumb-center gap-1">
+                            {item.title}
+                            <ChevronDownIcon />
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent align="start">
+                            {item.items?.map((item: any, index: number) => (
+                              <DropdownMenuItem key={index}>
+                                {item}
+                              </DropdownMenuItem>
+                            ))}
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </BreadcrumbItem>
+                    ) : (
+                      <BreadcrumbItem key={index}>{item.title}</BreadcrumbItem>
+                    )}
+                  </div>
+                </Fragment>
+              )}
             </Fragment>
           ))}
         </div>
 
-        <UserBadge user={user} />
-
+        <UserBadge
+          className={`hidden sm:flex `}
+          variant="minimalist"
+          user={user}
+        />
       </div>
     </div>
   );
