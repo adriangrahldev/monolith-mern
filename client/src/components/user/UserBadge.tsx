a8032947a51644dd78606609efc837634e1a14e0
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-29 03:21:46 -0400

    Agregar mejoras de diseño responsivo a UserBadge y corregir errores

--- client/src/components/user/UserBadge.tsx
+++ client/src/components/user/UserBadge.tsx
@@ -23,14 +23,14 @@ const UserBadge = ({
             </span>
           </div>
         ) : (
-          <Skeleton className="w-[43px] h-[43px] bg-black rounded-full flex items-center justify-center"></Skeleton>
+          <Skeleton className={`w-[43px] h-[43px] bg-black rounded-full flex items-center justify-center ${className || ""}`}></Skeleton>
         )}
       </div>
     );
   }
 
   return user ? (
-    <div         className={`flex gap-2 items-center justify-center rounded-md bg-[#eeeeee] px-3 py-2 h-[55px] ${className}`}>
+    <div         className={`flex gap-2 items-center justify-center rounded-md bg-[#eeeeee] px-3 py-2 h-[55px] ${className || ""}`}>
       <div className="w-[43px] h-[43px] bg-black rounded-full flex items-center justify-center">
         <span className="text-white font-bold text-lg">
           {user?.name[0] || ""}
@@ -42,7 +42,7 @@ const UserBadge = ({
       </div>
     </div>
   ) : (
-    <div className="flex gap-2 items-center justify-between rounded-md bg-[#eeeeee] px-4 py-2 w-[250px] h-[55px] ">
+    <div className={`flex gap-2 items-center justify-between rounded-md bg-[#eeeeee] px-4 py-2 w-[250px] h-[55px] ${className || ""}`}>
       <Skeleton className="w-[43px] h-[43px] bg-black rounded-full flex items-center justify-center"></Skeleton>
       <div className="flex flex-col gap-2 flex-1">
         <Skeleton className="w-[55px] h-[10px] rounded-full" />
