b64ef5a31f351d4c95f3ced53c6d9c01e998f744
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-29 03:21:18 -0400

    Agregar mejoras de diseño responsivo al SideBar y hacer responsivo

--- client/src/components/ui/SideBar.tsx
+++ client/src/components/ui/SideBar.tsx
@@ -1,33 +1,67 @@
-'use client'
+"use client"
+import { faClose } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBorderAll, faProjectDiagram, faUsers } from "@fortawesome/free-solid-svg-icons";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useBreadcrumb } from '@/contexts/BreadcrumbContext';
+import UserBadge from "../user/UserBadge";
+import { Button } from "./button";
+import Image from "next/image";
 
-
-const menuItems = [
-  { name: "Dashboard", path: "/dashboard", icon: faBorderAll },
-  { name: "Projects", path: "/projects", icon: faProjectDiagram },
-  { name: "Clients", path: "/clients", icon: faUsers },
-];
-
-export const SideBar = ({ routes }: { routes: any[] }) => {
+export const SideBar = ({
+  routes,
+  toggleSidebar,
+  showingSidebar,
+}: {
+  routes: any[];
+  toggleSidebar?: CallableFunction;
+  showingSidebar?: boolean;
+}) => {
   const pathname = usePathname();
-  const currentPath = '/' + pathname.split('/')[1];
+  const currentPath = "/" + pathname.split("/")[1];
 
   return (
-    <aside className="fixed w-20 bg-white-800 h-screen border-gray-300 border-r-2">
+    <aside
+      className={`fixed w-48 md:w-20 sm:w-20 bg-white-800 h-screen border-gray-300 border-r-2 bg-white z-10 max-lg:shadow-lg ${
+        !showingSidebar ? "max-lg:hidden" : ""
+      } `}
+    >
       <div className="flex items-center justify-center h-20 bg-white-900 border-b-2">
-        <Image src="/isologo1.svg" alt="Monolith Logo" width={32} height={32} />
-      </div >
-      <nav >
-        <ul>
+        <Image
+          src="/isologo1.svg"
+          alt="Monolith Logo"
+          width={32}
+          height={32}
+          className="max-lg:hidden"
+        />
+        <div className="hidden max-lg:flex justify-end items-center">
+          <Image
+            src="/logotipo1.png"
+            alt="Monolith Logo"
+            width={128}
+            height={64}
+          />
+          <Button
+            variant={"ghost"}
+            size={"default"}
+            onClick={(e) => toggleSidebar!()}
+          >
+            <FontAwesomeIcon icon={faClose} />
+          </Button>
+        </div>
+      </div>
+      <nav className="flex flex-col">
+        <ul className="flex-1">
           {routes.map((item) => (
-            <li key={item.title} className={`flex items-center justify-center h-20 ${currentPath === item.path ? 'bg-white-700 text-black' : 'text-gray-400'}`}>
+            <li
+              key={item.title}
+              className={`flex items-center justify-center max-lg:justify-start h-20 ${
+                currentPath === item.path
+                  ? "bg-white-700 text-black"
+                  : "text-gray-400"
+              }`}
+            >
               <Link href={item.path}>
-                <div className="flex flex-col items-center w-full h-full justify-center cursor-pointer">
+                <div className="flex flex-col max-lg:flex-row gap-2 sm:gap-0 md:gap-0  w-full h-full items-center cursor-pointer max-lg:px-8">
                   <FontAwesomeIcon icon={item.icon} size="lg" />
                   <span className="text-xs mt-2">{item.title}</span>
                 </div>
@@ -36,6 +70,12 @@ export const SideBar = ({ routes }: { routes: any[] }) => {
           ))}
         </ul>
       </nav>
+      <div className="absolute bottom-0 w-full h-28 flex flex-col  justify-center items-center">
+        <UserBadge user={{name: "Adrian Grahl",email: "adriangrahldev@gmail.com"}} variant="minimalist" className="hidden max-lg:flex" />
+        <Link href={'/user/login'} >
+        Sign Out
+        </Link>
+      </div>
     </aside>
   );
 };
