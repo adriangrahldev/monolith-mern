240a82b6d165389b2fe632ec2f8a1a23ceedb12f
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-29 01:37:23 -0400

    Mejorar la implementación de Breadcrums

--- client/src/components/ui/TopBar.tsx
+++ client/src/components/ui/TopBar.tsx
@@ -10,7 +10,10 @@ import {
 import { BreadcrumbItem as BCItem } from "@/contexts/BreadcrumbItem.interface";
 import { useBreadcrumb } from "@/contexts/BreadcrumbContext";
 import UserBadge from "../user/UserBadge";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight, faCircleDot } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const TopBar = () => {
   const { breadcrumb, addItem, removeItem } = useBreadcrumb();
@@ -28,33 +31,40 @@ const TopBar = () => {
   return (
     <div className="w-full pr-20 h-20 ml-20 fixed bg-white border-b-2">
       <div className="h-20 flex justify-between items-center px-6 ">
-        <div className="topLeft flex gap-2 text-2xl">
+        <div className="topLeft flex gap-2 items-center text-2xl">
+          <div className={`font-bold text-black`}>
+            Dashboard
+          </div>
           {breadcrumb.map((item: BCItem, index: number) => (
-            <div key={index} className={`${index === 0 ? 'font-bold' : ''}`}>
-              {item.type === "link" ? (
-                <BreadcrumbItem key={index}>
-                  {item.icon}
-                  {item.title}
-                </BreadcrumbItem>
-              ) : item.type === "dropdown" ? (
-                <BreadcrumbItem key={index}>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger className="flex breadcrumb-center gap-1">
-                      {item.title}
-                      <ChevronDownIcon />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="start">
-                      {item.items?.map((item: any, index: number) => (
-                        <DropdownMenuItem key={index}>{item}</DropdownMenuItem>
-                      ))}
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </BreadcrumbItem>
-              ) : (
-                <BreadcrumbItem key={index}>{item.title}</BreadcrumbItem>
-              )}
-              {index < breadcrumb.length - 1 && <>/</>}
-            </div>
+            <Fragment key={index}>
+              <span><FontAwesomeIcon icon={faChevronRight} width={8}  /></span>
+              <div className={`font-normal text-lg text-gray-600`}>
+                {item.type === "link" ? (
+                  <BreadcrumbItem key={index}>
+                    <Link href={item.link || '/dashboard'}>
+                    {item.icon}
+                    {item.title}
+                    </Link>
+                  </BreadcrumbItem>
+                ) : item.type === "dropdown" ? (
+                  <BreadcrumbItem key={index}>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger className="flex breadcrumb-center gap-1">
+                        {item.title}
+                        <ChevronDownIcon />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="start">
+                        {item.items?.map((item: any, index: number) => (
+                          <DropdownMenuItem key={index}>{item}</DropdownMenuItem>
+                        ))}
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </BreadcrumbItem>
+                ) : (
+                  <BreadcrumbItem key={index}>{item.title}</BreadcrumbItem>
+                )}
+              </div>
+            </Fragment>
           ))}
         </div>
 
