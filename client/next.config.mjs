88590f7400bccfc3bed3ae7b02ab3e9487bd5fcf
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-05-07 19:07:39 -0400

    Add lazy loading for Gravatar images in next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "s.gravatar.com",
+        port: "",
+        pathname: "/avatar/**",
+      }
     ],
   },
 };
