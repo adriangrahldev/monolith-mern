cff7a9367290d650914fe20360e8ba6a24fdddc5
José Ferreira <jf561428@gmail.com>
2024-05-07 09:11:18 -0400

    refactor: Update client route to include signup functionality

--- client/src/app/api/auth/[auth0]/route.ts
+++ client/src/app/api/auth/[auth0]/route.ts
@@ -1,10 +1,10 @@
-import {
-  handleAuth,
-  handleLogin,
-} from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
 export const GET = handleAuth({
   login: handleLogin({
     returnTo: "/dashboard",
+  }),
+  signup: handleLogin({
+    authorizationParams: { screen_hint: "signup" },
   }),
 });
