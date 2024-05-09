a33e2838486462d7b121c2a657be3d390cf00843
José Ferreira <jf561428@gmail.com>
2024-05-09 16:02:48 -0400

    refactor: Add isDeleted field to client model and update middleware for filtering deleted clients

--- server/src/models/client.model.ts
+++ server/src/models/client.model.ts
@@ -6,18 +6,49 @@ export interface IClient extends Document {
   phone: string;
   userId: string;
   avatar: string;
+  isDeleted: boolean;
   createdAt: Date;
   updatedAt: Date;
   projectsCounter: number;
 }
 
-export const clientSchema = new Schema<IClient>({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true },
-  userId: { type: String, required: true },
-  avatar: { type: String },
-  projectsCounter: { type: Number, default: 0 },
-}, { timestamps: true });
+export const clientSchema = new Schema<IClient>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    userId: { type: String, required: true },
+    avatar: { type: String },
+    isDeleted: { type: Boolean, default: false },
+    projectsCounter: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+//middleware para solo traer los completados
+clientSchema.pre("find", function (next) {
+  this.where({ isDeleted: false });
+  next();
+});
+
+clientSchema.pre("findOne", function (next) {
+  this.where({ isDeleted: false });
+  next();
+});
+
+clientSchema.pre("findOneAndUpdate", function (next) {
+  this.where({ isDeleted: false });
+  next();
+});
+
+clientSchema.pre("updateOne", function (next) {
+  this.where({ isDeleted: false });
+  next();
+});
+
+clientSchema.pre("updateMany", function (next) {
+  this.where({ isDeleted: false });
+  next();
+});
 
 export default model<IClient>("Client", clientSchema);
