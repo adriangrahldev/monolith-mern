81b3c99626e6c190891ed93c6283d4a7915ccb6e
José Ferreira <jf561428@gmail.com>
2024-05-09 16:01:04 -0400

    chore: Remove user-related code and dependencies

--- server/src/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as userController from '../controllers/user.controller';
-
-const router = express.Router();
-
-router.post('/register', userController.registerUser);
-router.post('/login', userController.loginUser);
-
-export default router;
