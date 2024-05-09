eda2029e00408c4193b1b242e9f7f0ea330ef0b4
José Ferreira <jf561428@gmail.com>
2024-05-09 16:04:50 -0400

    refactor: Add isDeleted field to project model and update middleware for filtering deleted projects

--- server/src/models/project.model.ts
+++ server/src/models/project.model.ts
@@ -1,22 +1,24 @@
 import { Document, Schema, model } from "mongoose";
-
-
+import Client, { IClient } from "./client.model";
 // Define the project schema
 export interface IProject extends Document {
-    userId: string;
-    client: any;
-    name: string;
-    description: string;
-    image: string;
-    startDate: Date;
-    endDate: Date;
-    status: string;
-    tasksCounter: number;
-    createdAt: Date;
-    updatedAt: Date;
+  userId: string;
+  client: any;
+  name: string;
+  description: string;
+  image: string;
+  startDate: Date;
+  endDate: Date;
+  status: string;
+  tasksCounter: number;
+  isDeleted: boolean;
+  completedTasksCounter: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-export const projectSchema = new Schema<IProject>({
+export const projectSchema = new Schema<IProject>(
+  {
     userId: { type: String, required: true },
     client: { type: Schema.Types.ObjectId, ref: "Client", required: true },
     name: { type: String, required: true },
@@ -25,7 +27,58 @@ export const projectSchema = new Schema<IProject>({
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
     tasksCounter: { type: Number, default: 0 },
+    completedTasksCounter: { type: Number, default: 0 },
+    isDeleted: { type: Boolean, default: false },
     status: { type: String, required: true },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
+
+// Middleware para incrementar contador de proyectos del cliente
+projectSchema.post("save", function (doc: IProject, next) {
+  Client.findByIdAndUpdate(doc.client, {
+    $inc: { projectsCounter: 1 },
+  })
+    .exec()
+    .then(() => next())
+    .catch((err) => next(err));
+});
+
+// Middleware para decrementar el contador de proyectos del cliente cuando se elimina un proyecto
+projectSchema.post("findOneAndUpdate", function (doc: IProject, next) {
+  if (doc.isDeleted) {
+    Client.findByIdAndUpdate(doc.client, {
+      $inc: { projectsCounter: -1 },
+    })
+      .exec()
+      .then(() => {
+        next();
+      })
+      .catch((err) => next(err));
+  } else {
+    next();
+  }
+});
+
+//solo devolver los que no estan eliminados
+projectSchema.pre("find", function (next) {
+  this.where({ isDeleted: false });
+  next();
+});
+
+projectSchema.pre("findOne", function (next) {
+  this.where({ isDeleted: false });
+  next();
+});
+
+projectSchema.pre("findOneAndUpdate", function (next) {
+  this.where({ isDeleted: false });
+  next();
+});
+
+projectSchema.pre("updateOne", function (next) {
+  this.where({ isDeleted: false });
+  next();
+});
 
 export default model<IProject>("Project", projectSchema);
