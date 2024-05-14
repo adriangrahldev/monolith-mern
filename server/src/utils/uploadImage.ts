ab9b7eb533a4dfd13f3ebf690fa4a4c75ff7cbaf
ivan <jf561428@gmail.com>
2024-05-14 11:20:38 -0400

    feat: Add utility function for uploading images with Firebase storage

---
+++ server/src/utils/uploadImage.ts
@@ -0,0 +1,49 @@
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { storage } from "../config/firebase.config";
+import sharp from "sharp";
+
+export const uploadFile = async (file: Express.Multer.File) => {
+  try {
+    if (!file.buffer) {
+      throw new Error("File buffer is missing");
+    }
+    const validMimeTypes = [
+      "image/jpeg",
+      "image/png",
+      "image/webp",
+      "image/tiff",
+      "image/gif",
+      "image/svg+xml",
+    ];
+    if (!validMimeTypes.includes(file.mimetype)) {
+      throw new Error("Unsupported file type");
+    }
+
+    const fileBuffer = await sharp(file.buffer)
+      .resize(800, 800, {
+        fit: "inside",
+      })
+      .toFormat("webp")
+      .webp({ quality: 80 })
+      .toBuffer();
+
+    const fileRef = ref(storage, `file/${file.originalname} ${Date.now()}`);
+    const fileMetadata = {
+      contentType: file.mimetype,
+    };
+
+    const fileUploadPromise = uploadBytesResumable(
+      fileRef,
+      fileBuffer,
+      fileMetadata
+    );
+    await fileUploadPromise;
+
+    const fileDownloadURL = await getDownloadURL(fileRef);
+
+    return { ref: fileRef, downloadUrl: fileDownloadURL };
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw error;
+  }
+};
