666159a80429ee62b45d68526b58b54c9defcc7f
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-05-15 17:13:33 -0400

    refactor: Update AdminHomePage to fetch pending tasks from API

--- client/src/app/(projects)/dashboard/page.tsx
+++ client/src/app/(projects)/dashboard/page.tsx
@@ -1,12 +1,35 @@
 "use client"
-import DefaultCard from "@/components/admin/DefaultCard";
-import TaskTable from "@/components/tasks/TaskTable";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { useBreadcrumb } from "@/contexts/BreadcrumbContext";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import {useRouter} from "next/navigation";
+import DefaultCard from "@/components/admin/DefaultCard";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import TaskTable from "@/components/tasks/TaskTable";
 
 const AdminHomePage = () => {
+  const router = useRouter();
+
   const { clearItems } = useBreadcrumb();
+
+  const fetchPendingTasks = useCallback(async () => {
+      try {
+        const res = await fetch(`/api/tasks?status=in-backlog`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+  
+        if (res.status === 401) {
+          router.push("/api/auth/login");
+        }
+
+        const data = await res.json();
+        setPendingTasks(data);
+      } catch (error) {
+        throw error;
+      }
+  }, [router]);
 
 
   useEffect(() => {
@@ -18,41 +41,8 @@ const AdminHomePage = () => {
   const [pendingTasks, setPendingTasks] = useState<any>([])
 
   useEffect(() => {
-    setPendingTasks([
-      {
-        id: 1,
-        description: "Design new homepage",
-        dateFrom: "24 May 2024, 10:00",
-        dateTo: "27 May 2024, 09:30",
-        project: "Web Design",
-        status: "Not started"
-      },
-      {
-        id: 2,
-        description: "Design services page",
-        dateFrom: "28 May 2024, 10:00",
-        dateTo: "30 May 2024, 09:30",
-        project: "Web Design",
-        status: "Not started"
-      },
-      {
-        id: 3,
-        description: "Design about us page",
-        dateFrom: "31 May 2024, 10:00",
-        dateTo: "4 June 2024, 09:30",
-        project: "Web Design",
-        status: "Not started"
-      },
-      {
-        id: 4,
-        description: "Design contact us page",
-        dateFrom: "5 June 2024, 10:00",
-        dateTo: "7 June 2024, 09:30",
-        project: "Web Design",
-        status: "Not started"
-      },
-    ])
-  }, []);
+    fetchPendingTasks();
+  }, [fetchPendingTasks]);
   return (
     <div className="flex flex-col gap-8">
       <div className="grid  gap-4 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3">
