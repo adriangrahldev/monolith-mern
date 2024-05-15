4e8b450679a2ac4f359fc827ab21976cbfd59ba2
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-05-15 19:46:27 -0400

    feat: Fetch and display the number of projects and clients on the Admin Home Page

--- client/src/app/(projects)/dashboard/page.tsx
+++ client/src/app/(projects)/dashboard/page.tsx
@@ -6,14 +6,68 @@ import DefaultCard from "@/components/admin/DefaultCard";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import TaskTable from "@/components/tasks/TaskTable";
 import TableSkeleton from '../../../components/skeletons/TableSkeleton';
+import DefaultCardSkeleton from "@/components/skeletons/DefaultCardSkeleton";
 
 const AdminHomePage = () => {
   const router = useRouter();
 
   const { clearItems } = useBreadcrumb();
   const [pendingTasks, setPendingTasks] = useState<any>([])
+  const [projectsCount, setProjectsCount] = useState<number>(0)
+  const [clientsCount, setClientsCount] = useState<number>(0)
+  
   const [fetchingTasks, setFetchingTasks] = useState<boolean>(false)
+  const [fetchingProjects, setFetchingProjects] = useState<boolean>(false)
+  const [fetchingClients, setFetchingClients] = useState<boolean>(false)
 
+  const fetchProjects = useCallback(async () => {
+    setFetchingProjects(true);
+      try {
+        const res = await fetch(`/api/projects`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+  
+        if (res.status === 401) {
+          router.push("/api/auth/login");
+        }
+
+        const data = await res.json();
+        setProjectsCount(data.length);
+        setFetchingProjects(false);
+      } catch (error) {
+        setFetchingProjects(false);
+        throw error;
+
+      }
+  }, [router])
+
+  const fetchClients = useCallback(async () => {
+    setFetchingClients(true);
+      try {
+        const res = await fetch(`/api/clients`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+  
+        if (res.status === 401) {
+          router.push("/api/auth/login");
+        }
+
+        const data = await res.json();
+        setClientsCount(data.length);
+        setFetchingClients(false);
+      } catch (error) {
+        setFetchingClients(false);
+        throw error;
+
+      }
+  }, [router])
+  
   const fetchPendingTasks = useCallback(async () => {
     setFetchingTasks(true);
       try {
@@ -41,7 +95,6 @@ const AdminHomePage = () => {
 
   useEffect(() => {
     clearItems();
-
   }, [clearItems])
 
 
@@ -49,11 +102,35 @@ const AdminHomePage = () => {
   useEffect(() => {
     fetchPendingTasks();
   }, [fetchPendingTasks]);
+
+  useEffect(() => {
+    fetchProjects();
+  }, [fetchProjects]);
+
+  useEffect(() => {
+    fetchClients();
+  }, [fetchClients]);
+
   return (
     <div className="flex flex-col gap-8">
       <div className="grid  gap-4 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3">
-        <DefaultCard title="Clients" counter={2} link="/clients" />
-        <DefaultCard title="Projects" counter={7} link="/projects" />
+        
+      {
+        fetchingProjects && <DefaultCardSkeleton />
+      }
+
+      {
+        !fetchingProjects && <DefaultCard title="Projects" counter={projectsCount} link="/projects" />
+      }
+
+      {
+        fetchingClients && <DefaultCardSkeleton />
+      }
+
+      {
+        !fetchingClients && <DefaultCard title="Clients" counter={clientsCount} link="/clients" />
+      }
+
       </div>
 
       <hr />
