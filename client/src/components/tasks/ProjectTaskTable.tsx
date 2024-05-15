266741ac7b71188e550f1733a18e30eb92ebc65b
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-05-15 00:07:53 -0400

    refactor: Update ProjectTaskTable component to include task status update functionality and improve UI

--- client/src/components/tasks/ProjectTaskTable.tsx
+++ client/src/components/tasks/ProjectTaskTable.tsx
@@ -1,10 +1,14 @@
 import { Task } from "@/interfaces/task";
-import { Table, TableCaption, TableHeader, TableRow, TableHead, TableBody, TableCell } from "../ui/table";
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "../ui/table";
 import moment from "moment";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
-const ProjectTaskTable = ({tasks, statusFilter}:{
+const ProjectTaskTable = ({tasks, statusFilter, handleTaskUpdate}:{
     tasks: Task[],
-    statusFilter: string
+    statusFilter: string,
+    handleTaskUpdate: CallableFunction
 }) => {
 
 
@@ -21,22 +25,21 @@ const ProjectTaskTable = ({tasks, statusFilter}:{
         }
     }
 
-
     return (
         <Table>
             <TableHeader>
                 <TableRow>
-                    <TableHead className="w-[35%]">Description</TableHead>
-                    <TableHead className="w-[20%]">Start Date</TableHead>
-                    <TableHead className="w-[20%]">End Date</TableHead>
-                    <TableHead className="w-[15%]">Status</TableHead>
-                    <TableHead className="w-[15%]">Actions</TableHead>
+                    <TableHead className="w-[35%] font-bold">Description</TableHead>
+                    <TableHead className="w-[20%] font-bold">Start Date</TableHead>
+                    <TableHead className="w-[20%] font-bold">End Date</TableHead>
+                    <TableHead className="w-[15%] font-bold">Status</TableHead>
+                    <TableHead className="w-[15%] font-bold">Actions</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody className="overflow-scroll">
                 {tasks.map((task: Task, index:number) => (
                     statusFilter === "all" || task.status === statusFilter ? (
-                        <TableRow key={task._id} className={` hover:shadow-xl hover:bg-slate-300 cursor-pointer ${index % 2 === 0 ? 'bg-slate-200' : ''}`}>
+                        <TableRow key={task._id} className={` hover:shadow-xl hover:bg-slate-200 cursor-pointer ${index % 2 === 0 ? 'bg-slate-100' : ''}`}>
                             <TableCell>{task.description}</TableCell>
                             <TableCell>
                                 {
@@ -48,9 +51,23 @@ const ProjectTaskTable = ({tasks, statusFilter}:{
                                     task.endDate ? moment(task.endDate).format("MMM DD, YYYY") : "No End Date"
                                 }
                             </TableCell>
-                            <TableCell>{getStatus(task.status)}</TableCell>
                             <TableCell>
-                                <button className="btn btn-primary">Start</button>
+                                <Select onValueChange={(value:string) => {handleTaskUpdate(task,value)}} value={task.status}>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Status" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="in-backlog">In Backlog</SelectItem>
+                                        <SelectItem value="in-progress">In Progress</SelectItem>
+                                        <SelectItem value="completed">Completed</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </TableCell>
+                            <TableCell>
+                                <div className="flex gap-2 justify-center">
+                                    <FontAwesomeIcon icon={faEdit} />
+                                    <FontAwesomeIcon icon={faTrashAlt} />
+                                </div>
                             </TableCell>
                         </TableRow>
                     ) : null
