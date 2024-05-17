6c7271ac4ec826ea36f0dd9f2168b77d80b88c0d
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-05-17 11:59:29 -0400

    feat: Remove unnecessary code in ProjectPage component

--- client/src/app/(projects)/projects/[id]/page.tsx
+++ client/src/app/(projects)/projects/[id]/page.tsx
@@ -210,7 +210,6 @@ const ProjectPage = () => {
           router.push("/api/auth/login");
         }
         fetchTasks();
-        toggleCreateTaskModal();
         fetchProject();
       } catch (error) {
         console.log(error);
