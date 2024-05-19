1023ad151e9f8129ea5eb4291b2f135884e25c58
José Ferreira <jf561428@gmail.com>
2024-05-18 22:56:00 -0400

    chore: Update project routes to handle image uploads

--- server/src/routes/project.routes.ts
+++ server/src/routes/project.routes.ts
@@ -5,16 +5,27 @@ import {
   getProject,
   createProject,
   updateProject,
-  deleteProject,  
+  deleteProject,
 } from "../controllers/project.controller";
 import { jwtCheck } from "../middleware/authMiddleware";
+import { upload } from "../config/multer";
 
 const router = Router();
 
 router.get("/", jwtCheck, getProjects);
-router.post("/", jwtCheck, createProject);
+router.post(
+  "/",
+  jwtCheck,
+  upload.fields([{ name: "image", maxCount: 1 }]),
+  createProject
+);
 router.get("/:id", jwtCheck, getProject);
-router.put("/:id", jwtCheck, updateProject);
+router.put(
+  "/:id",
+  jwtCheck,
+  upload.fields([{ name: "image", maxCount: 1 }]),
+  updateProject
+);
 router.delete("/:id", jwtCheck, deleteProject);
 
 
