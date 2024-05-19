3acec825034577cd8668b9c1a391db3f7b780a90
José Ferreira <jf561428@gmail.com>
2024-05-18 22:58:54 -0400

    refactor: Add image field to Task interface

--- client/src/interfaces/task.ts
+++ client/src/interfaces/task.ts
@@ -1,12 +1,13 @@
 export interface Task {
-    _id?: string;
-    title: string;
-    description?: string;
-    status: "in-progress" | "completed" | "in-backlog";
-    startDate: string;
-    endDate: string;
-    projectId: string;
-    projectName?: string;
-    createdAt?: string;
-    updatedAt?: string;
+  _id?: string;
+  title: string;
+  description?: string;
+  status: "in-progress" | "completed" | "in-backlog";
+  startDate: string;
+  endDate: string;
+  projectId: string;
+  projectName?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  image?: string;
 }
