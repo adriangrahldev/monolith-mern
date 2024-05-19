8d1dada57498073ba97968dadc097c002bbadc70
José Ferreira <jf561428@gmail.com>
2024-05-18 22:58:07 -0400

    refactor: Update project routes to handle image uploads

--- server/src/routes/task.routes.ts
+++ server/src/routes/task.routes.ts
@@ -5,17 +5,27 @@ import {
   getTask,
   createTask,
   updateTask,
-  deleteTask,  
+  deleteTask,
 } from "../controllers/task.controller";
 import { jwtCheck } from "../middleware/authMiddleware";
+import { upload } from "../config/multer";
 
 const router = Router();
 
 router.get("/", jwtCheck, getTasks);
 router.get("/:id", jwtCheck, getTask);
-router.post("/", jwtCheck, createTask);
-router.put("/:id", jwtCheck, updateTask);
+router.post(
+  "/",
+  jwtCheck,
+  upload.fields([{ name: "image", maxCount: 1 }]),
+  createTask
+);
+router.put(
+  "/:id",
+  jwtCheck,
+  upload.fields([{ name: "image", maxCount: 1 }]),
+  updateTask
+);
 router.delete("/:id", jwtCheck, deleteTask);
-
 
 export default router;
