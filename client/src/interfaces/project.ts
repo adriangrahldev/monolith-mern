3a8120ceb1ac2b54c117085730e9acb3a30b4119
ivan <jf561428@gmail.com>
2024-05-25 01:23:23 -0400

    feat: Add demo URL field to Project interface

--- client/src/interfaces/project.ts
+++ client/src/interfaces/project.ts
@@ -3,20 +3,21 @@ import { Comment } from "./comment";
 import { Task } from "./task";
 
 export interface Project {
-    _id?: string;
-    userId: string;
-    client: string | Client;
-    name: string;
-    description: string;
-    image?: string;
-    startDate: string;
-    endDate: string;
-    status: "pending" | "in-progress" | "completed";
-    createdAt?: Date;
-    updatedAt?: Date;
-    tasksCounter?: number;
-    comments?: Comment[];
-    tasks?: Task[];
-    completedTasksCounter?: number;
-    isOnline?: boolean;
+  _id?: string;
+  userId: string;
+  client: string | Client;
+  name: string;
+  description: string;
+  image?: string;
+  startDate: string;
+  endDate: string;
+  status: "pending" | "in-progress" | "completed";
+  createdAt?: Date;
+  updatedAt?: Date;
+  tasksCounter?: number;
+  comments?: Comment[];
+  tasks?: Task[];
+  completedTasksCounter?: number;
+  isOnline?: boolean;
+  demoUrl?: string;
 }
