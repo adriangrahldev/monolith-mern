4c0718be0cba2525215bbb46ef3c1aa5a1306b0d
ivan <jf561428@gmail.com>
2024-05-25 00:58:55 -0400

    feat: remove cache from project view

--- client/src/app/api/view/route.ts
+++ client/src/app/api/view/route.ts
@@ -1,20 +1,20 @@
-
-
 import { NextRequest, NextResponse } from "next/server";
 const apiURL = process.env.API_SERVER_URL;
 
 export async function GET(req: NextRequest) {
-    if (req.url.includes("projectId")) {
-        const projectId = req.url.split("=")[1];
-        try {
-            const response = await fetch(`${apiURL}/api/projects/view/${projectId}`);
-            console.log(response.url);
-            return NextResponse.json(await response.json());
-        } catch (error) {
-            console.error("Failed to fetch project:", error);
-            return NextResponse.json({ message: "Server error" }, { status: 500 });
-        }
-    } else {
-        return NextResponse.json({ message: "Invalid request" }, { status: 400 });
+  if (req.url.includes("projectId")) {
+    const projectId = req.url.split("=")[1];
+    try {
+      const response = await fetch(`${apiURL}/api/projects/view/${projectId}`, {
+        cache: "no-store", //desactiva el cache ya que no se actualiza el detalle del proyecto
+      });
+      console.log(response.url);
+      return NextResponse.json(await response.json());
+    } catch (error) {
+      console.error("Failed to fetch project:", error);
+      return NextResponse.json({ message: "Server error" }, { status: 500 });
     }
+  } else {
+    return NextResponse.json({ message: "Invalid request" }, { status: 400 });
+  }
 }
